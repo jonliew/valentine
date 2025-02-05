f6330d72cbcd097ade5a0bfd776bc55bd0588f53
Jonathan Liew <j0nathanl13w@gmail.com>
2025-02-05 04:43:19 -0600

    Adjust countdown unit for smaller screens

--- src/components/CountdownUnit.tsx
+++ src/components/CountdownUnit.tsx
@@ -6,7 +6,7 @@ interface IProps {
 export function CountdownUnit(props: IProps) {
 	const { value, label } = props;
 	return (
-		<div className="flex flex-col items-center border-4 rounded-md w-24 py-4">
+		<div className="flex flex-col items-center border-4 rounded-md md:w-24 w-16 py-4">
 			<div className="text-4xl font-bold">{value}</div>
 			<div className="text-sm">{label}</div>
 		</div>
